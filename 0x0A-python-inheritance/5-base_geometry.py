71737c2d6ef2ac8d0af99a54adeca2fd648c03fa
Kwekuzee <kwekuamuahodum@gmail.com>
2023-04-11 16:08:56 -0700

    foeks

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+class BaseGeometry:
+    """Represents base geometry."""
+
+    def area(self):
+        """Raise an exception."""
+        raise Exception("area() is not implemented")
+
+    def perimeter(self):
+        """Raise an exception."""
+        raise Exception("perimeter() is not implemented")
+
